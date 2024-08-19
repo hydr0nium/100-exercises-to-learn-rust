2b1b8b279df0bba9b5ea3e9dd1ff3ec70cda29cb
hydr0nium <37932436+hydr0nium@users.noreply.github.com>
2024-08-19 13:30:35 +0200

    Finished calculator while

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,14 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    if n == 0 {
+        return 1;
+    }
+    let mut sum = 1;
+    for i in 1..=n {
+        sum *= i;
+    }
+    sum
 }
 
 #[cfg(test)]
